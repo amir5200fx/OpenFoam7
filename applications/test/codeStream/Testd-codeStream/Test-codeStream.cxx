22f461c42ed80c8d65c77ab251c8290f83ee6cec
PayvandH <Peivandhabibi@gmail.com>
2021-10-13 20:48:07 +0330

    "codeStream" has been added to "test"

---
+++ applications/test/codeStream/Testd-codeStream/Test-codeStream.cxx
@@ -0,0 +1,76 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    Test-codeStream
+
+Description
+
+\*---------------------------------------------------------------------------*/
+
+#include <includeAllModules.hxx>
+
+#include <argList.hxx>
+#include <IOstreams.hxx>
+#include <IOobject.hxx>
+#include <IFstream.hxx>
+#include <dictionary.hxx>
+
+using namespace tnbLib;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+//  Main program:
+
+int main(int argc, char* argv[])
+{
+    argList::validArgs.insert("dict .. dictN");
+    argList args(argc, argv, false, true);
+
+    Info << nl
+        << "FOAM_CASE=" << getEnv("FOAM_CASE") << nl
+        << "FOAM_CASENAME=" << getEnv("FOAM_CASENAME") << nl
+        << endl;
+
+    if (args.size() <= 1)
+    {
+        Info << "specify dictionaries to test\n";
+    }
+    else
+    {
+        IOobject::writeDivider(Info);
+        for (label argI = 1; argI < args.size(); ++argI)
+        {
+            const string& dictFile = args[argI];
+            IFstream is(dictFile);
+
+            dictionary dict(is);
+
+            Info << dict << endl;
+        }
+    }
+
+    return 0;
+}
+
+
+// ************************************************************************* //
