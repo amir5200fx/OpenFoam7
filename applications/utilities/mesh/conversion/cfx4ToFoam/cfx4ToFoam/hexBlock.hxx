e3ea396de6202245ee74e97d4a82782359828000
PayvandH <Peivandhabibi@gmail.com>
2021-11-08 22:55:44 +0330

    "cfxToFoam" has been added to "conversion" directory of "mesh"

---
+++ applications/utilities/mesh/conversion/cfx4ToFoam/cfx4ToFoam/hexBlock.hxx
@@ -0,0 +1,165 @@
+#pragma once
+#ifndef _hexBlock_Header
+#define _hexBlock_Header
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Class
+    Foam::hexBlock
+
+Description
+    Hex block definition used in the cfx converter.
+
+SourceFiles
+    hexBlock.C
+
+\*---------------------------------------------------------------------------*/
+
+#include <includeAllModules.hxx>
+
+#include <labelList.hxx>
+#include <pointField.hxx>
+#include <faceList.hxx>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace tnbLib
+{
+
+    /*---------------------------------------------------------------------------*\
+                               Class hexBlock Declaration
+    \*---------------------------------------------------------------------------*/
+
+    class hexBlock
+    {
+        // Private Data
+
+            //- Handedness of the block
+        enum handed
+        {
+            noPoints,
+            right,
+            left
+        };
+
+        //- Number of point in each direction
+        label xDim_;
+        label yDim_;
+        label zDim_;
+
+        //- Handedness of the block
+        handed blockHandedness_;
+
+        //- List of points
+        pointField points_;
+
+
+        // Private Member Functions
+
+            //- Vertex addressing inside the block
+        inline label vtxLabel(label i, label j, label k) const;
+
+
+    public:
+
+        // Constructors
+
+            //- Construct from components
+        hexBlock(const label nx, const label ny, const label nz);
+
+        //- Disallow default bitwise copy construction
+        hexBlock(const hexBlock&) = delete;
+
+
+        // Member Functions
+
+            //- Number of points
+        label xDim() const
+        {
+            return xDim_;
+        }
+
+        label yDim() const
+        {
+            return yDim_;
+        }
+
+        label zDim() const
+        {
+            return zDim_;
+        }
+
+        label nBlockPoints() const
+        {
+            return (xDim_ + 1) * (yDim_ + 1) * (zDim_ + 1);
+        }
+
+        label nBlockCells() const
+        {
+            return xDim_ * yDim_ * zDim_;
+        }
+
+        //- Return block points
+        const pointField& points() const
+        {
+            if (blockHandedness_ == noPoints)
+            {
+                FatalErrorInFunction
+                    << "points not read in yet"
+                    << abort(FatalError);
+            }
+
+            return points_;
+        }
+
+        //- Return block cells
+        labelListList blockCells() const;
+
+        //- Return block patch faces given direction and range limits
+        // From the cfx manual: direction
+        // 0 = solid (3-D patch),
+        // 1 = high i, 2 = high j, 3 = high k
+        // 4 = low i, 5 = low j, 6 = low k
+        faceList patchFaces(label direc, const labelList& range) const;
+
+
+        //- Read block points
+        void readPoints(Istream&);
+
+
+        // Member Operators
+
+            //- Disallow default bitwise assignment
+        void operator=(const hexBlock&) = delete;
+    };
+
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#endif
+
+// ************************************************************************* //
