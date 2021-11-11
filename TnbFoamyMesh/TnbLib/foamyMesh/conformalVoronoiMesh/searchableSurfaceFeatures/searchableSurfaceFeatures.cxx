e794c8a3855854657974889912557574c7d0404c
PayvandH <Peivandhabibi@gmail.com>
2021-11-11 14:42:28 +0330

    "searchableSurfaceFeatures" has been added to "conformalVoronoiMesh" directory of "foamyMesh" in "generation" directory of "mesh"

---
+++ TnbFoamyMesh/TnbLib/foamyMesh/conformalVoronoiMesh/searchableSurfaceFeatures/searchableSurfaceFeatures.cxx
@@ -0,0 +1,87 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2013-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include <searchableSurfaceFeatures.hxx>
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace tnbLib
+{
+    defineTypeNameAndDebug(searchableSurfaceFeatures, 0);
+    defineRunTimeSelectionTable(searchableSurfaceFeatures, dict);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+tnbLib::autoPtr<tnbLib::searchableSurfaceFeatures>
+tnbLib::searchableSurfaceFeatures::New
+(
+    const searchableSurface& surface,
+    const dictionary& dict
+)
+{
+    word searchableSurfaceFeaturesType = surface.type() + "Features";
+
+    dictConstructorTable::iterator cstrIter =
+        dictConstructorTablePtr_->find(searchableSurfaceFeaturesType);
+
+    if (cstrIter == dictConstructorTablePtr_->end())
+    {
+        FatalErrorInFunction
+            << "Unknown searchableSurfaceFeatures type "
+            << searchableSurfaceFeaturesType << endl << endl
+            << "Valid searchableSurfaceFeatures types : " << endl
+            << dictConstructorTablePtr_->sortedToc()
+            << exit(FatalError);
+    }
+
+    return autoPtr<searchableSurfaceFeatures>(cstrIter()(surface, dict));
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+tnbLib::searchableSurfaceFeatures::searchableSurfaceFeatures
+(
+    const searchableSurface& surface,
+    const dictionary& dict
+)
+    :
+    surface_(surface),
+    dict_(dict)
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+tnbLib::searchableSurfaceFeatures::~searchableSurfaceFeatures()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+
+// ************************************************************************* //
