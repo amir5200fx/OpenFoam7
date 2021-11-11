d47d7d5b5396a0694bbe9a41bd9700da40af8b55
PayvandH <Peivandhabibi@gmail.com>
2021-11-11 14:40:33 +0330

    "faceAreaWeightModel" has been added to "conformalVoronoiMesh" directory of "foamyMesh" in "generation" directory of "mesh"

---
+++ TnbFoamyMesh/TnbLib/foamyMesh/conformalVoronoiMesh/faceAreaWeightModel/faceAreaWeightModel/faceAreaWeightModel.hxx
@@ -0,0 +1,141 @@
+#pragma once
+#ifndef _faceAreaWeightModel_Header
+#define _faceAreaWeightModel_Header
+
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2012-2019 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Class
+    tnbLib::faceAreaWeightModel
+
+Description
+    Abstract base class for providing faceAreaWeight values to the cell motion
+    controller based on an argument faceAreaFraction value, typically the ratio
+    of the area of the face in question to the area of a square face with a side
+    length of the local target cell size.
+
+SourceFiles
+    faceAreaWeightModel.C
+
+\*---------------------------------------------------------------------------*/
+
+#include <FoamyMesh_Module.hxx>
+
+#include <point.hxx>
+#include <dictionary.hxx>
+#include <autoPtr.hxx>
+#include <runTimeSelectionTables.hxx>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace tnbLib
+{
+
+    /*---------------------------------------------------------------------------*\
+                           Class faceAreaWeightModel Declaration
+    \*---------------------------------------------------------------------------*/
+
+    class faceAreaWeightModel
+        :
+        public dictionary
+    {
+
+    protected:
+
+        // Protected data
+
+            //- Method coeffs dictionary
+        dictionary coeffDict_;
+
+
+    public:
+
+        //- Runtime type information
+        TypeName("faceAreaWeightModel");
+
+
+        // Declare run-time constructor selection table
+
+        declareRunTimeSelectionTable
+        (
+            autoPtr,
+            faceAreaWeightModel,
+            dictionary,
+            (
+                const dictionary& faceAreaWeightDict
+                ),
+            (faceAreaWeightDict)
+        );
+
+
+        // Constructors
+
+            //- Construct from components
+        faceAreaWeightModel
+        (
+            const word& type,
+            const dictionary& faceAreaWeightDict
+        );
+
+        //- Disallow default bitwise copy construction
+        faceAreaWeightModel(const faceAreaWeightModel&) = delete;
+
+
+        // Selectors
+
+            //- Return a reference to the selected faceAreaWeightModel
+        static autoPtr<faceAreaWeightModel> New
+        (
+            const dictionary& faceAreaWeightDict
+        );
+
+
+        //- Destructor
+        virtual ~faceAreaWeightModel();
+
+
+        // Member Functions
+
+            //- Const access to the coeffs dictionary
+        const dictionary& coeffDict() const
+        {
+            return coeffDict_;
+        }
+
+        //- Return the current faceAreaWeight coefficient
+        virtual scalar faceAreaWeight(scalar faceAreaFraction) const = 0;
+
+
+        // Member Operators
+
+            //- Disallow default bitwise assignment
+        void operator=(const faceAreaWeightModel&) = delete;
+    };
+
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace tnbLib
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+#endif // !_faceAreaWeightModel_Header
---
+++ TnbFoamyMesh/TnbLib/foamyMesh/conformalVoronoiMesh/faceAreaWeightModel/piecewiseLinearRamp/piecewiseLinearRamp.hxx
@@ -0,0 +1,97 @@
+#pragma once
+#ifndef _piecewiseLinearRamp_Header
+#define _piecewiseLinearRamp_Header
+
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2012-2019 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Class
+    tnbLib::piecewiseLinearRamp
+
+Description
+    A linear ramp between 0 and 1 with definable start and end points.
+
+SourceFiles
+    piecewiseLinearRamp.C
+
+\*---------------------------------------------------------------------------*/
+
+#include <faceAreaWeightModel.hxx>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace tnbLib
+{
+
+    /*---------------------------------------------------------------------------*\
+                               Class piecewiseLinearRamp Declaration
+    \*---------------------------------------------------------------------------*/
+
+    class piecewiseLinearRamp
+        :
+        public faceAreaWeightModel
+    {
+
+    private:
+
+        // Private Data
+
+            //- Face area fraction below which a weight of 0 is returned
+        scalar lAF_;
+
+        //- Face area fraction above which a which of 1 is returned
+        scalar uAF_;
+
+    public:
+
+        //- Runtime type information
+        TypeName("piecewiseLinearRamp");
+
+        // Constructors
+
+            //- Construct from components
+        piecewiseLinearRamp
+        (
+            const dictionary& faceAreaWeightDict
+        );
+
+
+        //- Destructor
+        virtual ~piecewiseLinearRamp()
+        {}
+
+
+        // Member Functions
+
+            //- Return the faceAreaWeight
+        virtual scalar faceAreaWeight(scalar faceAreaFraction) const;
+    };
+
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace tnbLib
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#endif // !_piecewiseLinearRamp_Header
