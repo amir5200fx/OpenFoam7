8b4ed21ecb089c3da9fb6eb82379f56b7740d78b
PayvandH <Peivandhabibi@gmail.com>
2021-11-19 00:33:31 +0330

    "buoyantPimpleFoam" has been added to "solvers\heatTransfer\buoyantPimpleFoam"

---
+++ applications/solvers/heatTransfer/buoyantPimpleFoam/buoyantPimpleFoam.cxx
@@ -0,0 +1,113 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    buoyantPimpleFoam
+
+Description
+    Transient solver for buoyant, turbulent flow of compressible fluids for
+    ventilation and heat-transfer.
+
+    Turbulence is modelled using a run-time selectable compressible RAS or
+    LES model.
+
+\*---------------------------------------------------------------------------*/
+
+#include <includeAllModules.hxx>
+
+#include <fvCFD.hxx>
+#include <rhoThermo.hxx>
+#include <turbulentFluidThermoModel.hxx>
+#include <radiationModel.hxx>
+#include <fvOptions.hxx>
+#include <pimpleControl.hxx>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char* argv[])
+{
+#include <postProcess.lxx>
+
+#include <setRootCaseLists.lxx>
+#include <createTime.lxx>
+#include <createMesh.lxx>
+#include <createControl.lxx>
+#include "createFields.lxx"
+#include "createFieldRefs.lxx"
+#include <initContinuityErrs.lxx>
+#include <createTimeControls.lxx>
+#include <compressibleCourantNo.lxx>
+#include <setInitialDeltaT.lxx>
+
+    turbulence->validate();
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info << "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+#include <readTimeControls.lxx>
+#include <compressibleCourantNo.lxx>
+#include <setDeltaT.lxx>
+
+        runTime++;
+
+        Info << "Time = " << runTime.timeName() << nl << endl;
+
+#include <rhoEqn.lxx>
+
+        // --- Pressure-velocity PIMPLE corrector loop
+        while (pimple.loop())
+        {
+#include "UEqn.lxx"
+#include "EEqn.lxx"
+
+            // --- Pressure corrector loop
+            while (pimple.correct())
+            {
+#include "pEqn.lxx"
+            }
+
+            if (pimple.turbCorr())
+            {
+                turbulence->correct();
+            }
+        }
+
+        rho = thermo.rho();
+
+        runTime.write();
+
+        Info << "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info << "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
