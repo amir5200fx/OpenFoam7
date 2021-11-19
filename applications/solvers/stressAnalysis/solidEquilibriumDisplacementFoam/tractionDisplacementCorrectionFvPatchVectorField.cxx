6f5c537781a225d183b592890c95794c9b9b9758
PayvandH <Peivandhabibi@gmail.com>
2021-11-19 17:45:41 +0330

    "solidEquilibriumDisplacementFoam" has been added to "applications\solvers\stressAnalysis" but still not compilable

---
+++ applications/solvers/stressAnalysis/solidEquilibriumDisplacementFoam/tractionDisplacementCorrectionFvPatchVectorField.cxx
@@ -0,0 +1,212 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "tractionDisplacementCorrectionFvPatchVectorField.hxx"
+
+#include <addToRunTimeSelectionTable.hxx>
+#include <volFields.hxx>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace tnbLib
+{
+
+    // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+    tractionDisplacementCorrectionFvPatchVectorField::
+        tractionDisplacementCorrectionFvPatchVectorField
+        (
+            const fvPatch& p,
+            const DimensionedField<vector, volMesh>& iF
+        )
+        :
+        fixedGradientFvPatchVectorField(p, iF),
+        traction_(p.size(), Zero),
+        pressure_(p.size(), 0.0)
+    {
+        fvPatchVectorField::operator=(patchInternalField());
+        gradient() = Zero;
+    }
+
+
+    tractionDisplacementCorrectionFvPatchVectorField::
+        tractionDisplacementCorrectionFvPatchVectorField
+        (
+            const tractionDisplacementCorrectionFvPatchVectorField& tdpvf,
+            const fvPatch& p,
+            const DimensionedField<vector, volMesh>& iF,
+            const fvPatchFieldMapper& mapper
+        )
+        :
+        fixedGradientFvPatchVectorField(tdpvf, p, iF, mapper),
+        traction_(mapper(tdpvf.traction_)),
+        pressure_(mapper(tdpvf.pressure_))
+    {}
+
+
+    tractionDisplacementCorrectionFvPatchVectorField::
+        tractionDisplacementCorrectionFvPatchVectorField
+        (
+            const fvPatch& p,
+            const DimensionedField<vector, volMesh>& iF,
+            const dictionary& dict
+        )
+        :
+        fixedGradientFvPatchVectorField(p, iF),
+        traction_("traction", dict, p.size()),
+        pressure_("pressure", dict, p.size())
+    {
+        fvPatchVectorField::operator=(patchInternalField());
+        gradient() = Zero;
+    }
+
+
+    tractionDisplacementCorrectionFvPatchVectorField::
+        tractionDisplacementCorrectionFvPatchVectorField
+        (
+            const tractionDisplacementCorrectionFvPatchVectorField& tdpvf
+        )
+        :
+        fixedGradientFvPatchVectorField(tdpvf),
+        traction_(tdpvf.traction_),
+        pressure_(tdpvf.pressure_)
+    {}
+
+
+    tractionDisplacementCorrectionFvPatchVectorField::
+        tractionDisplacementCorrectionFvPatchVectorField
+        (
+            const tractionDisplacementCorrectionFvPatchVectorField& tdpvf,
+            const DimensionedField<vector, volMesh>& iF
+        )
+        :
+        fixedGradientFvPatchVectorField(tdpvf, iF),
+        traction_(tdpvf.traction_),
+        pressure_(tdpvf.pressure_)
+    {}
+
+
+    // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+    void tractionDisplacementCorrectionFvPatchVectorField::autoMap
+    (
+        const fvPatchFieldMapper& m
+    )
+    {
+        fixedGradientFvPatchVectorField::autoMap(m);
+        m(traction_, traction_);
+        m(pressure_, pressure_);
+    }
+
+
+    // Reverse-map the given fvPatchField onto this fvPatchField
+    void tractionDisplacementCorrectionFvPatchVectorField::rmap
+    (
+        const fvPatchVectorField& ptf,
+        const labelList& addr
+    )
+    {
+        fixedGradientFvPatchVectorField::rmap(ptf, addr);
+
+        const tractionDisplacementCorrectionFvPatchVectorField& dmptf =
+            refCast<const tractionDisplacementCorrectionFvPatchVectorField>(ptf);
+
+        traction_.rmap(dmptf.traction_, addr);
+        pressure_.rmap(dmptf.pressure_, addr);
+    }
+
+
+    // Update the coefficients associated with the patch field
+    void tractionDisplacementCorrectionFvPatchVectorField::updateCoeffs()
+    {
+        if (updated())
+        {
+            return;
+        }
+
+        const dictionary& mechanicalProperties = db().lookupObject<IOdictionary>
+            (
+                "mechanicalProperties"
+                );
+
+        const fvPatchField<scalar>& rho =
+            patch().lookupPatchField<volScalarField, scalar>("rho");
+
+        const fvPatchField<scalar>& rhoE =
+            patch().lookupPatchField<volScalarField, scalar>("E");
+
+        const fvPatchField<scalar>& nu =
+            patch().lookupPatchField<volScalarField, scalar>("nu");
+
+        scalarField E(rhoE / rho);
+        scalarField mu(E / (2.0 * (1.0 + nu)));
+        scalarField lambda(nu * E / ((1.0 + nu) * (1.0 - 2.0 * nu)));
+
+        Switch planeStress(mechanicalProperties.lookup("planeStress"));
+
+        if (planeStress)
+        {
+            lambda = nu * E / ((1.0 + nu) * (1.0 - nu));
+        }
+
+        vectorField n(patch().nf());
+
+        const fvPatchField<symmTensor>& sigmaD =
+            patch().lookupPatchField<volSymmTensorField, symmTensor>("sigmaD");
+
+        const fvPatchField<tensor>& sigmaExp =
+            patch().lookupPatchField<volTensorField, tensor>("sigmaExp");
+
+        gradient() =
+            (
+                (traction_ + pressure_ * n) / rho - (n & (sigmaD + sigmaExp))
+                ) / (2.0 * mu + lambda);
+
+        fixedGradientFvPatchVectorField::updateCoeffs();
+    }
+
+
+    void tractionDisplacementCorrectionFvPatchVectorField::write(Ostream& os) const
+    {
+        fvPatchVectorField::write(os);
+        writeEntry(os, "traction", traction_);
+        writeEntry(os, "pressure", pressure_);
+        writeEntry(os, "value", *this);
+    }
+
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    makePatchTypeField
+    (
+        fvPatchVectorField,
+        tractionDisplacementCorrectionFvPatchVectorField
+    );
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace tnbLib
+
+// ************************************************************************* //
