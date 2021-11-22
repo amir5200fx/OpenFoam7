205039078ecc3ae7ce9ad133eb472172b54f14e7
PayvandH <Peivandhabibi@gmail.com>
2021-11-22 21:44:56 +0330

    "XiFoam" has been added to "applications\solvers\combustion"

---
+++ applications/solvers/combustion/XiFoam/XiFoam.cxx
@@ -0,0 +1,142 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    XiFoam
+
+Description
+    Solver for compressible premixed/partially-premixed combustion with
+    turbulence modelling.
+
+    Combusting RANS code using the b-Xi two-equation model.
+    Xi may be obtained by either the solution of the Xi transport
+    equation or from an algebraic expression.  Both approaches are
+    based on Gulder's flame speed correlation which has been shown
+    to be appropriate by comparison with the results from the
+    spectral model.
+
+    Strain effects are encorporated directly into the Xi equation
+    but not in the algebraic approximation.  Further work need to be
+    done on this issue, particularly regarding the enhanced removal rate
+    caused by flame compression.  Analysis using results of the spectral
+    model will be required.
+
+    For cases involving very lean Propane flames or other flames which are
+    very strain-sensitive, a transport equation for the laminar flame
+    speed is present.  This equation is derived using heuristic arguments
+    involving the strain time scale and the strain-rate at extinction.
+    the transport velocity is the same as that for the Xi equation.
+
+\*---------------------------------------------------------------------------*/
+
+#include <includeAllModules.hxx>
+
+#include <fvCFD.hxx>
+#include <psiuReactionThermo.hxx>
+#include <turbulentFluidThermoModel.hxx>
+#include <laminarFlameSpeed.hxx>
+#include <ignition.hxx>
+#include <Switch.hxx>
+#include <pimpleControl.hxx>
+#include <fvOptions.hxx>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char* argv[])
+{
+#include <postProcess.lxx>
+
+#include <setRootCaseLists.lxx>
+#include <createTime.lxx>
+#include <createMesh.lxx>
+#include <createControl.lxx>
+
+#include "readCombustionProperties.lxx"
+#include "createFields.lxx"
+#include "createFieldRefs.lxx"
+
+#include <initContinuityErrs.lxx>
+#include <createTimeControls.lxx>
+#include <compressibleCourantNo.lxx>
+#include <setInitialDeltaT.lxx>
+
+    turbulence->validate();
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info << "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+#include <readTimeControls.lxx>
+#include <compressibleCourantNo.lxx>
+#include <setDeltaT.lxx>
+
+        runTime++;
+        Info << "Time = " << runTime.timeName() << nl << endl;
+
+#include <rhoEqn.lxx>
+
+        // --- Pressure-velocity PIMPLE corrector loop
+        while (pimple.loop())
+        {
+#include "UEqn.lxx"
+
+#include "ftEqn.lxx"
+#include "bEqn.lxx"
+#include "EauEqn.lxx"
+#include "EaEqn.lxx"
+
+            if (!ign.ignited())
+            {
+                thermo.heu() == thermo.he();
+            }
+
+            // --- Pressure corrector loop
+            while (pimple.correct())
+            {
+#include "pEqn.lxx"
+            }
+
+            if (pimple.turbCorr())
+            {
+                turbulence->correct();
+            }
+        }
+
+        rho = thermo.rho();
+
+        runTime.write();
+
+        Info << "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info << "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
